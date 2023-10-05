0a8517d878a6748cd80da976a9b3beafbadf890d
Mario1515 <126467500+Mario1515@users.noreply.github.com>
2023-10-05 16:39:15 +0300

    Login / Logout / Registered - Finished
    Login / Logout / Registered - Finished

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const express = require("express");
 const handlebars = require("express-handlebars");
 const path = require("path");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
 //imports from folders
+const { auth } = require("./middlewares/authMiddleware");
 const routes = require("./routes");
 
 //app
@@ -26,6 +28,8 @@ app.set("views", "src/views");
 // app use.
 app.use(express.static(path.resolve(__dirname, "public")));
 app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(auth);
 app.use(routes);
 
 app.listen(PORT, console.log(`Server is listening on port 3000...`));
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 
 const homeController = require("./controllers/homeController");
 const userController = require("./controllers/userController");
+const cryptoController = require("./controllers/cryptoController");
 
 router.use(homeController);
 router.use("/users", userController);
+router.use("/crypto", cryptoController);
 
 module.exports = router;
 
